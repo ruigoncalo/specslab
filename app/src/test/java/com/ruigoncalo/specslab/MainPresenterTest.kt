918e78d9f823b17c55339281abb9b6f6eb241d1f
ruigoncalo <ruimiguelgoncalo@gmail.com>
2017-11-22 23:37:16 +0000

    Update junit test

--- app/src/test/java/com/ruigoncalo/specslab/MainPresenterTest.kt
+++ app/src/test/java/com/ruigoncalo/specslab/MainPresenterTest.kt
@@ -47,10 +47,40 @@ class MainPresenterTest {
 
     @Test
     fun if_permission_denied_request_permission() {
+        whenever(runtimePermissionDelegate.checkPermission()).thenReturn(Observable.just(false))
         tested.start()
 
         verify(runtimePermissionDelegate).requestPermissions()
     }
 
+    @Test
+    fun if_granted_requested_permission_get_distances() {
+        tested.onPermissionGranted()
+
+        verify(distancesUseCase).distances()
+    }
+
+    @Test
+    fun if_denied_requested_permission_ask_for_rationale() {
+        tested.onPermissionDenied()
+
+        verify(runtimePermissionDelegate).shouldShowRationale()
+    }
+
+    @Test
+    fun on_positive_reply_for_rationale_show_rationale() {
+        whenever(runtimePermissionDelegate.shouldShowRationale()).thenReturn(Observable.just(true))
+        tested.onPermissionDenied()
+        
+        verify(view).showRationale()
+    }
+
+    @Test
+    fun on_negative_reply_for_rationale_show_settings() {
+        whenever(runtimePermissionDelegate.shouldShowRationale()).thenReturn(Observable.just(false))
+        tested.onPermissionDenied()
+
+        verify(view).showSettings()
+    }
 
 }
